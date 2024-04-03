4d5b879d458807c04e20d9e24b3bb915c83dec63
Javi <javi13@gmail.com>
2024-04-03 12:59:36 +0200

    Ej. 35 empleados

--- clase/35_empleados_clase/src/view/Empleados.java
+++ clase/35_empleados_clase/src/view/Empleados.java
@@ -6,6 +6,21 @@ import service.EmpleadosService;
 
 public class Empleados {
 	
+	/**
+	 * Realizar un programa para la gestión de empleados de una empresa. Un empleado se caracteriza por un nombre, y tiene asociado un código de empleado.
+		al iniciar el programa, aparecerá el siguiente menú:
+		1.- Nuevo empleado
+		2.- Buscar empleado
+		3.- Eliminar empleado
+		4.- Mostrar nombres de todos los empleados
+		5.- Salir
+		
+		1: Se pide el nómbre y código del empleado. Si no existe empleado con ese código, se guarda, pero si el código ya existe, no se guarda y se muestra advertencia al usuario
+		2: Se pide el código del empleado y se muestra su nombre
+		3: Se pìde el código del empleado y se borra, indicando un mensaje que diga el nombre del empleado que se ha borrado
+		4.- Muestra nombres de todos los empleados 
+	 */
+	
 	static EmpleadosService service = new EmpleadosService();
 	
 	public static void main(String[] args) {
