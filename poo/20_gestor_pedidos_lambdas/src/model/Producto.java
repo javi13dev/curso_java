8cb9b2ca1c3b53fddd918a3da044c5ebbb954249
Javi <javi13@gmail.com>
2024-04-18 12:41:11 +0200

    add nuevas funciones

---
+++ poo/20_gestor_pedidos_lambdas/src/model/Producto.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Producto {
+	
+	/**
+	 *  Programa para gestión de productos.
+
+		Cada producto tendrá: nombre, precio, categoria
+		
+		La lógica de negocio, expondrá los siguientes métodos:
+		
+		-eliminarPorCategoria: elimina de la lista aquellos productos que pertenezcan a la categoría indicada
+		
+		-bajarPrecio: Baja el precio de todos productos en el porcentaje indicado
+		
+		-subirPrecioCategoria: Sube el precio a todos los productos de la categoría indicada, en el porcentaje indicado
+		
+		-obtenerProductos: Devuelve la lista de productos existentes
+		-agregarProducto: Añade el producto recibido 
+	 */
+	private String nombre;
+	private double precio;
+	public String categoria;
+	
+	
+	public Producto(String nombre, double precio, String categoria) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
+	public Producto() {
+		super();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	
+}
---
+++ poo/20_gestor_pedidos_lambdas/src/service/ProductoService.java
@@ -0,0 +1,63 @@
+package service;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Producto;
+
+public class ProductoService {
+	List<Producto> lst = new ArrayList<Producto>();
+
+	/**
+	 *  Programa para gestión de productos.
+
+		Cada producto tendrá: nombre, precio, categoria
+		
+		La lógica de negocio, expondrá los siguientes métodos:
+		
+		-eliminarPorCategoria: elimina de la lista aquellos productos que pertenezcan a la categoría indicada
+		
+		-bajarPrecio: Baja el precio de todos productos en el porcentaje indicado
+		
+		-subirPrecioCategoria: Sube el precio a todos los productos de la categoría indicada, en el porcentaje indicado
+		
+		-obtenerProductos: Devuelve la lista de productos existentes
+		-agregarProducto: Añade el producto recibido 
+	 */
+	
+	public void eliminarPorCategoria( String categoria) {
+		
+		lst.removeIf(p -> p.getCategoria().equals(categoria));	
+	}
+	
+	public void bajarPrecio(int porcentaje) {
+		lst.forEach(p -> p.setPrecio(p.getPrecio() - p.getPrecio()*porcentaje/100));
+	}
+	
+	public void subirPrecioCategoria(int porcentaje, String categoria) {
+		
+		lst.replaceAll(p -> {
+			if(p.getCategoria().equals(categoria)) {
+				 p.setPrecio(p.getPrecio() + p.getPrecio()*porcentaje/100);
+			}
+			return p;
+		});
+		
+		lst.forEach(p -> p.setPrecio(p.getPrecio() + p.getPrecio()*porcentaje/100));
+		
+	}
+	
+	
+	public List<Producto> obtenerProductos(){
+		return lst;
+	}
+	
+	public void altaProdcuto(Producto producto){
+		lst.add(producto);
+	}
+	 
+	
+
+	
+	
+
+}
