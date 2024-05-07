d17cab5e68accd951023c23fa135cb6b778aa65b
Javi <javi13@gmail.com>
2024-05-07 13:57:56 +0200

    update cajero

--- java_estandar/21_cajero_electronico/src/service/CajeroServiceImpl.java
+++ java_estandar/21_cajero_electronico/src/service/CajeroServiceImpl.java
@@ -1,5 +1,47 @@
 package service;
 
-public class CajeroServiceImpl {
+public class CajeroServiceImpl implements CajeroService{
+
+	@Override
+	public Cuenta obtenerCuenta(int idCuenta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cuenta extraccion(int idCuenta, double cantidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cuenta ingreso(int idCuenta, double cantidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void transferencia(int idCuentaOrigen, int idCuentaDestino, double cantidad) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Cliente> obtenerTitulares(int idCuenta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Movimiento> obtenerMovimientos(int idCuenta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double obtenerSaldo(int idCuenta) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
